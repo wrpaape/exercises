bed68f2d5718b49afdf79a1e581cdda14bef555c
Reid Paape <wrpaape@gmail.com>
2015-11-11 20:13:17 -0600

    pmap

--- processes/multi_process.exs
+++ processes/multi_process.exs
@@ -5,15 +5,30 @@ defmodule MultiProcess do
     exit :cant_take_it_anymore
   end
 
-  def run do
-    {child_pid, _ref} = spawn_monitor(MultiProcess, :child, [self])
-    
+  def raise_child do
+    Process.flag(:trap_exit, true)
+    child_pid = spawn_link(MultiProcess, :child, [self])
+
     :timer.sleep 500
 
     receive do
       {^child_pid, msg} -> IO.puts msg
-    after 1000 ->
-      IO.puts "*crickets chirping*"
+      after 1000        -> IO.puts "*crickets chirping*"
     end
+    
+    receive do
+      {:EXIT, ^child_pid, reason} -> IO.puts "exited bc #{reason}"
+    end
+  end
+
+  def pmap(collection, fun) do
+    parent = self
+    collection
+    |> Enum.map(fn(elem) ->
+         spawn_link(fn -> send(parent, {self, fun.(elem)}) end)
+       end)
+    |> Enum.map(fn(pid) ->
+         receive do {^pid, result} -> result end
+       end)
   end
 end

61b6120372221aea3a6da9bc62d41cd16cca69ee
Reid Paape <wrpaape@gmail.com>
2015-11-12 12:38:01 -0600

    timer tocks sequentially, handling new clients registering async

--- processes/ticker.ex
+++ processes/ticker.ex
@@ -15,10 +15,10 @@ defmodule Ticker do
   def generator(todo, done) do
     receive do
       {:register, pid} ->
-        IO.puts "registering #{inspect pid}"
+        IO.puts "registering #{inspect pid}\n\n"
         generator(todo, [pid | done])
       {:do_tick} ->
-        IO.puts "tick"
+        IO.puts "** tick **"
         send_tick(todo, done)
     end
   end
@@ -32,23 +32,21 @@ defmodule Ticker do
     generator(todo, [next | done])
   end
 
-  def send_tick([], [next | todo]) do
+  def send_tick([], []) do
+    IO.puts "tock: no clients!"
+    generator([], [])
+  end
+
+  def send_tick([], done) do
+    [next | todo] =
+      done
+      |> Enum.reverse
     IO.puts "tock: #{inspect next}"
     IO.puts "todo: #{inspect todo}"
     IO.puts "done: []"
 
     send(next, {:tick})
-    todo
-    |> Enum.reverse
-    |> generator([next])
-  end
-
-  def send_tick(todo, done) do
-    IO.puts "tock: no clients!"
-    IO.puts "todo: #{inspect todo}"
-    IO.puts "done: #{inspect done}"
-
-    generator(todo, done)
+    generator(todo, [next])
   end
 end
 
@@ -61,7 +59,7 @@ defmodule Client do
   def receiver do
     receive do
       {:tick} ->
-        IO.puts "tock in client #{inspect self}"
+        IO.puts "tock in client #{inspect self}\n\n"
         receiver
     end
   end

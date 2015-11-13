f94f97cb6915e0deed423457e3c54ff0ab518d07
Reid Paape <wrpaape@gmail.com>
2015-11-12 18:18:03 -0600

    babby genserver

---
+++ sequence/lib/server.ex
@@ -0,0 +1,11 @@
+defmodule Sequence.Server do
+  use GenServer
+
+  def handle_call(:next_number, _from, current_number) do
+    {:reply, current_number, current_number + 1}
+  end
+
+  def handle_call(:pop, _from, [head | tail]) do
+    {:reply, head, tail}
+  end
+end

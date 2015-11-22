61c5c5f4c0afb30d888f1c8fa1e642d358a410e1
Reid Paape <wrpaape@gmail.com>
2015-11-22 15:54:02 -0600

    wooo macros

---
+++ macros/my.exs
@@ -0,0 +1,16 @@
+defmodule My do
+  defmacro macro(param) do
+    IO.inspect param
+  end
+
+  defmacro times_n(n) do
+
+    fun_name =
+      "times_"
+      <> Integer.to_string(n)
+      |> String.to_atom
+    quote do
+      def unquote(fun_name) (m), do: m * unquote(n)
+    end
+  end
+end

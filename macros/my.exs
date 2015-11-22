31160745cc606862db4613a15294678cb14556a8
Reid Paape <wrpaape@gmail.com>
2015-11-22 15:54:38 -0600

    wooo macros

--- macros/my.exs
+++ macros/my.exs
@@ -10,7 +10,7 @@ defmodule My do
       <> Integer.to_string(n)
       |> String.to_atom
     quote do
-      def unquote(fun_name) (m), do: m * unquote(n)
+      def unquote(fun_name) (m), do: m * n
     end
   end
 end
